cabbe421875ffe27c39c20bb785b0f2e41a8430e
VegetaS <18842636481@qq.com>
2018-03-23 00:08:31 +0800

    update CMake file

--- 3rd_party/3rd_party.macro.cmake
+++ 3rd_party/3rd_party.macro.cmake
@@ -1,12 +1,8 @@
 # =========== 3rd_party ===========
-unset (PROJECT_3RD_PARTY_SRC_LIST)
+set (PROJECT_3RD_PARTY_ROOT_DIR ${CMAKE_CURRENT_LIST_DIR})
 
 # =========== 3rd_party - protobuf ===========
-include("${PROJECT_3RD_PARTY_ROOT_DIR}/protobuf/protobuf.cmake")
-
-# =========== 3rd_party - muduo ===========
-include("${PROJECT_3RD_PARTY_ROOT_DIR}/muduo/muduo.cmake")
-
+#include("${PROJECT_3RD_PARTY_ROOT_DIR}/protobuf/protobuf.cmake")
 
 ## 导入所有工程项目
 add_project_recurse(${CMAKE_CURRENT_LIST_DIR})
